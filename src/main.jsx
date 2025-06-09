c8ccf12db8f21f96fe9726d1e06f5a6134fae758
iqbal-mn694 <garenahiber134@gmail.com>
2025-06-09 12:09:13 +0700

    fix import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Page from './routes/index.jsx'
 
