26657408ed3eecc52fd1f853bc5249647e8d642b
iqbal-mn694 <garenahiber134@gmail.com>
2025-06-09 12:12:30 +0700

    change font in hero banner

--- src/components/commons/atoms/Logo/index.jsx
+++ src/components/commons/atoms/Logo/index.jsx
@@ -2,12 +2,12 @@ const Logo = () => {
   return (
     <>
     <div class="flex items-center gap-2 font-semibold text-2xl">
-  <div class="w-10 h-10 rounded-full bg-gradient-to-br from-red-500 to-red-700 text-white flex items-center justify-center text-xl font-bold shadow-md">
-    E
-  </div>
-  
-  <span class="text-gray-800 tracking-wide font-serif max-md:hidden">Exclusiva</span>
-</div>
+      <div class="w-10 h-10 rounded-full bg-gradient-to-br from-red-500 to-red-700 text-white flex items-center justify-center text-xl font-bold shadow-md font-montserrat cursor-pointer">
+        E
+      </div>
+      
+      <span class="text-gray-800 tracking-wide font-montserrat max-md:hidden">Exclusiva</span>
+    </div>
 
     </>
   )
