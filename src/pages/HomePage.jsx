c491e90cdfe64210e757917bae0a1ce539836fdf
iqbal-mn694 <garenahiber134@gmail.com>
2025-06-09 12:08:35 +0700

    add Hero banner to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
+import HeroBanner from "../components/commons/molecules/HeroBanner"
+
 const HomePage = () => {
   return (
     <>
-      <h1>Ini homepage</h1>
+      <HeroBanner />
     </>
   )
 }
